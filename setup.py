335f5dc3decaa771680d84ecc09978bf011d549d
jce <jce@melexis.com>
2020-10-07 11:28:30 +0200

    Remove invalid Topic classifier

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Topic :: Automation',
         'Topic :: Documentation',
         'Topic :: Utilities',
     ],
