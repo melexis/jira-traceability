8115fbabdc0c326971e16e3ad17a75a6dce22cc6
JWM <jwm@melexis.com>
2026-03-24 13:45:15 +0100

    Make sure suffix is not used for empty attendees

--- mlx/jira_traceability/jira_interaction.py
+++ mlx/jira_traceability/jira_interaction.py
@@ -86,7 +86,8 @@ def create_unique_issues(item_ids, jira, general_fields, settings, traceability_
         if suffix:
             if assignee and '@' not in assignee:
                 assignee = f"{assignee}{suffix}".lower()
-            attendees = [f"{attendee}{suffix}".lower() if '@' not in attendee else attendee for attendee in attendees]
+            attendees = [f"{attendee}{suffix}".lower() if '@' not in attendee else attendee for attendee in attendees
+                         if attendee]
 
         jira_field_id = settings['jira_field_id']
         jira_field_query_value = escape_special_characters(jira_field)
